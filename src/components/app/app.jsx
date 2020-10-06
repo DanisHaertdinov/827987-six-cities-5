69f322e23dcd962fa5c66442ad331c35d2e08732
Danis Haertdiov <danis.haertdinov@gmail.com>
2020-10-06 21:06:00 +0400

    sets up routing

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,11 +1,30 @@
 import React from "react";
-import MainScreen from "../main-screen/main-screen";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
 import PropTypes from "prop-types";
+import MainScreen from "../main-screen/main-screen";
+import PropertyScreen from '../property-screen/property-screen';
+import LoginScreen from '../login-screen/login-screen';
+import FavoritesScreen from '../favorites-screen/favorites-screen';
 
 const App = ({offersCount}) => (
-  <MainScreen
-    offersCount = {offersCount}
-  />
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/">
+        <MainScreen
+          offersCount = {offersCount}
+        />
+      </Route>
+      <Route exact path="/login">
+        <LoginScreen/>
+      </Route>
+      <Route exact path="/favorites">
+        <FavoritesScreen/>
+      </Route>
+      <Route exact path="/offer/:id">
+        <PropertyScreen/>
+      </Route>
+    </Switch>
+  </BrowserRouter>
 );
 
 App.propTypes = {
