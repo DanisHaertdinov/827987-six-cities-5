a83e641ad8ef41253004cf9e5a78ce23e018522e
Danis Haertdiov <danis.haertdinov@gmail.com>
2020-10-11 21:53:50 +0400

    add offer-card component

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,6 @@
 export const OFFERS_COUNT = 312;
+
+export const RATING = {
+  MIN: 1,
+  MAX: 5,
+};
--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app";
 import {OFFERS_COUNT} from "./constants";
+import {offers} from './mocks/offers';
+import {reviews} from './mocks/reviews';
 
 ReactDOM.render(
     <App
       offersCount = {OFFERS_COUNT}
+      offers = {offers}
+      reviews = {reviews}
     />,
     document.querySelector(`#root`)
 );
