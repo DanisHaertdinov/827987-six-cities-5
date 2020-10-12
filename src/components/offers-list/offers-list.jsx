c86ee017aa575ca86109ef889bb316e0c3072f88
Danis Haertdiov <danis.haertdinov@gmail.com>
2020-10-12 20:28:11 +0400

    add offers-list component

---
+++ src/components/offers-list/offers-list.jsx
@@ -0,0 +1,31 @@
+import React, {PureComponent} from "react";
+import PropTypes from "prop-types";
+import OfferCard from "../offer-card/offer-card";
+
+class OffersList extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      activeOffer: null,
+    };
+  }
+
+  render() {
+    const {offers} = this.props;
+
+    return (
+      <div className="cities__places-list places__list tabs__content">
+        {offers.map((offer) => <OfferCard
+          key={offer.id}
+          offer={offer}
+          onMouseOver={() => this.setState({activeOffer: offer})}
+        />) }
+      </div>
+    );
+  }
+}
+
+OffersList.propTypes = {
+  offers: PropTypes.array.isRequired,
+};
